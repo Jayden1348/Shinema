20cba630470ec9fedb5955093a8ad05fbaa71da7
JAYDEN1348 <1071427@hr.nl>
2024-03-27 17:00:51 +0100

    overzicht stoelen en reserveren klaar

--- shinema/DataModels/SeatModel.cs
+++ shinema/DataModels/SeatModel.cs
@@ -1,15 +1,7 @@
-using System.Text.Json.Serialization;
-
-
 public class SeatModel
 {
-    [JsonPropertyName("rank")]
     public int Rank { get; set; }
-
-    [JsonPropertyName("position")]
     public string Position { get; set; }
-
-    [JsonPropertyName("available")]
     public bool Available { get; set; }
 
     public SeatModel(int rank, string position)
