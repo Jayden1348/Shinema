f37c19d8f233184a2777591bcf219968a94aff4c
JAYDEN1348 <1071427@hr.nl>
2024-04-16 11:21:44 +0200

    empty json solution

--- shinema/DataAccess/HallAccess.cs
+++ shinema/DataAccess/HallAccess.cs
@@ -8,6 +8,21 @@ static class HallAccess
     public static List<List<SeatModel>> LoadAll(int which_hall)
     {
         string json = File.ReadAllText(path);
+        List<List<SeatModel>> hall = new();
+        int check_empty = json.Count();
+        if (check_empty == 2)
+        {
+            WriteAll();
+            json = File.ReadAllText(path);
+        }
         return JsonSerializer.Deserialize<List<List<List<SeatModel>>>>(json)![which_hall - 1];
+    }
+
+    public static void WriteAll()
+    {
+        List<List<List<SeatModel>>> halls = new() { HallLogic.CreateMovieHall(1), HallLogic.CreateMovieHall(2), HallLogic.CreateMovieHall(3) };
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(halls, options);
+        File.WriteAllText(path, json);
     }
 }
