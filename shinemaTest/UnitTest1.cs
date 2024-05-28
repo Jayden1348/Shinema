27b21a1d272119b0e948b79d35895c3d78b31c4b
Aidan90009 <aidantsoi06@gmail.com>
2024-05-28 09:54:52 +0200

    added unittest for BarReservation

--- shinemaTest/UnitTest1.cs
+++ shinemaTest/UnitTest1.cs
@@ -269,6 +269,15 @@ public class UnitTest1
     [TestMethod]
     public void TestCinemaInfoTimeValidity()
     {
+        // return value description
+
+        // returns -4 if ":" is not in time string
+        // returns -3 if hours is not an integer
+        // returns -2 if minutes is not an integer
+        // returns -1 if hours is out of range (higher than 24 or lower than 0)
+        // returns 0 if minutes is out of range (higher than 59 or lower than 0)
+        // returns 1 if time string is correct
+
 
         // inputs to test that match expected output 
         string[] testInputs =  { "23:59", "23:60", "24:00",
@@ -332,5 +341,44 @@ public class UnitTest1
         //Check price
         Assert.IsTrue(FoodLogic.GetAllFood().Any(i => i.Price == price1));
     
+    }
+    
+    [TestMethod]
+    public void TestBarReservationAvailabilty()
+    {
+        BarReservationLogic barReservationLogic = new BarReservationLogic();
+        //First Test
+        DateTime newReservationDateTime = DateTime.Parse("20-5-2024 15:01");
+        DateTime ReservationDateTime1 = DateTime.Parse("20-5-2024 12:00");
+        DateTime ReservationDateTime2 = DateTime.Parse("20-5-2024 12:00");
+        List<BarReservationModel> reservationList = new List<BarReservationModel>{ new BarReservationModel(1, "A", ReservationDateTime1, 10),
+                                                                                   new BarReservationModel(2, "B", ReservationDateTime2, 20)
+        };
+        int openSeatsTest1 = barReservationLogic.CheckBarAvailability(newReservationDateTime, reservationList);
+
+        Assert.AreEqual(40, openSeatsTest1);
+
+        //Second Test
+        DateTime newReservationDateTimeTest2 = DateTime.Parse("20-5-2024 15:00");
+        DateTime ReservationDateTime1Test2 = DateTime.Parse("20-5-2024 12:01");
+        DateTime ReservationDateTime2Test2 = DateTime.Parse("20-5-2024 12:01");
+        List<BarReservationModel> reservationListTest2 = new List<BarReservationModel>{ new BarReservationModel(1, "A", ReservationDateTime1Test2, 10),
+                                                                                        new BarReservationModel(2, "B", ReservationDateTime2Test2, 20)
+        };
+        int openSeatsTest2 = barReservationLogic.CheckBarAvailability(newReservationDateTimeTest2, reservationListTest2);
+
+        Assert.AreEqual(10 , openSeatsTest2);
+
+        //Third Test
+        DateTime newReservationDateTimeTest3 = DateTime.Parse("20-5-2024 12:00");
+        DateTime ReservationDateTime1Test3 = DateTime.Parse("20-5-2024 12:00");
+        DateTime ReservationDateTime2Test3 = DateTime.Parse("20-5-2024 15:01");
+        List<BarReservationModel> reservationListTest3 = new List<BarReservationModel>{ new BarReservationModel(1, "A", ReservationDateTime1Test3, 10),
+                                                                                        new BarReservationModel(2, "B", ReservationDateTime2Test3, 20)
+        };
+        int openSeatsTest3 = barReservationLogic.CheckBarAvailability(newReservationDateTimeTest3, reservationListTest3);
+
+        Assert.AreEqual(30, openSeatsTest3);
+
     }
 }
