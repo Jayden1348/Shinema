10729c6caa7318034f4f4265ff4a5e04e96f47ee
C-G-Santos <1073607@hr.nl>
2024-05-30 16:28:05 +0200

    Testing Path Testing the Getfullpath on other device

--- shinema/DataAccess/GenericAccess.cs
+++ shinema/DataAccess/GenericAccess.cs
@@ -1,9 +1,13 @@
 using System.Text.Json;
 
-// public static class GenericAccess<T>
-// {
-//     private string GetFullPath(T type)
-//     {
-//         return "";
-//     }
-// }
+public static class GenericAccess<T>
+{
+    public static string GetFullPath()
+    {
+        Type t = typeof(T);
+        string name = t.Name.Substring(0, t.Name.Length - 5);
+        string location = "@DataSources/" + name + ".json";
+        string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, location));
+        return path;
+    }
+}
--- shinema/Program.cs
+++ shinema/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.Clear();
 Console.ResetColor();
+Console.WriteLine(GenericAccess<MovieModel>.GetFullPath());
 Console.WriteLine("Welcome to this amazing program");
 Console.WriteLine("\nPress any key to continue...");
 Console.ReadKey();
