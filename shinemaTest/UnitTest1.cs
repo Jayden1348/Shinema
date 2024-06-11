943f4852fff6ee38e11ae2b9266ae4a1d69cf12d
NoLimitJath <jathjackrich2@gmail.com>
2024-06-11 09:41:16 +0200

    ShowingID remove

--- shinemaTest/UnitTest1.cs
+++ shinemaTest/UnitTest1.cs
@@ -87,10 +87,10 @@ public class UnitTest1
     public void TestDeleteMovie()
     {
         // Create a new movie
-        MovieModel movie = new MovieModel(1, "Test Movie", 120, "16", "Test Description", 1, new List<string> { "Action" }, "2021-01-01");
+        MovieModel movie = new MovieModel(1, "Test Movie", 120, "16", "Test Description", new List<string> { "Action" }, "2021-01-01");
 
         // Add the movie to the list
-        MoviesLogic.AddMovie(movie.ID, movie.Title, movie.Length, movie.Age, movie.Description, movie.ShowingID, movie.Genre, movie.Release_Date);
+        MoviesLogic.AddMovie(movie.ID, movie.Title, movie.Length, movie.Age, movie.Description, movie.Genre, movie.Release_Date);
 
         // Delete the movie
         bool boolCheck = MoviesLogic.DeleteMovie(movie.ID);
@@ -269,7 +269,7 @@ public class UnitTest1
     public void TestCheckIfMovieExist()
     {
         string movieDiscription = "The Godfather \"Don\" Vito Corleone is the head of the Corleone mafia family in New York. He is at the event of his daughter's wedding. Michael, Vito's youngest son and a decorated WWII Marine is also present at the wedding. Michael seems to be uninterested in being a part of the family business. Vito is a powerful man, and is kind to all those who give him respect but is ruthless against those who do not. But when a powerful and treacherous rival wants to sell drugs and needs the Don's influence for the same, Vito refuses to do it. What follows is a clash between Vito's fading old values and the new ways which may cause Michael to do the thing he was most reluctant in doing and wage a mob war against all the other mafia families which could tear the Corleone family apart. ";
-        MovieModel newMovie = new MovieModel(1, "The Godfather", 185, "14", movieDiscription, 1, new List<string> { "Crime", "Drama" }, "1972");
+        MovieModel newMovie = new MovieModel(1, "The Godfather", 185, "14", movieDiscription, new List<string> { "Crime", "Drama" }, "1972");
         MoviesLogic.UpdateMovieList(newMovie);
         Assert.AreEqual(MoviesLogic.CheckIfMovieExist(newMovie.ID), newMovie);
     }
@@ -279,7 +279,7 @@ public class UnitTest1
     public void TestCheckIfMovieNotExist()
     {
         string movieDiscription = "The Godfather \"Don\" Vito Corleone is the head of the Corleone mafia family in New York. He is at the event of his daughter's wedding. Michael, Vito's youngest son and a decorated WWII Marine is also present at the wedding. Michael seems to be uninterested in being a part of the family business. Vito is a powerful man, and is kind to all those who give him respect but is ruthless against those who do not. But when a powerful and treacherous rival wants to sell drugs and needs the Don's influence for the same, Vito refuses to do it. What follows is a clash between Vito's fading old values and the new ways which may cause Michael to do the thing he was most reluctant in doing and wage a mob war against all the other mafia families which could tear the Corleone family apart. ";
-        MovieModel newMovie = new MovieModel(1, "The Godfather", 185, "14", movieDiscription, 1, new List<string> { "Crime", "Drama" }, "1972");
+        MovieModel newMovie = new MovieModel(1, "The Godfather", 185, "14", movieDiscription, new List<string> { "Crime", "Drama" }, "1972");
         MoviesLogic.UpdateMovieList(newMovie);
         Assert.AreEqual(MoviesLogic.CheckIfMovieExist(-1123), null);
     }
@@ -424,7 +424,7 @@ public class UnitTest1
     public void TestUpdateMovieList()
     {
         string movieDescription = "Inception is a 2010 science fiction action film written and directed by Christopher Nolan, who also produced the film with Emma Thomas. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets. He is offered a chance to have his criminal history erased as payment for the implantation of another person's idea into a target's subconscious.";
-        MovieModel newMovie = new MovieModel(1, "Inception", 148, "13", movieDescription, 1, new List<string> { "Action", "Sci-Fi", "Thriller" }, "2010");
+        MovieModel newMovie = new MovieModel(1, "Inception", 148, "13", movieDescription, new List<string> { "Action", "Sci-Fi", "Thriller" }, "2010");
 
         // Update the movie properties
         newMovie.Title = "The Godfather";
@@ -458,7 +458,7 @@ public class UnitTest1
     {
         ShowingsLogic showings = new ShowingsLogic();
         int future = (DateTime.Now.AddYears(100)).Year;
-        MovieModel m = new MovieModel(1, "TestMovie", 60, null, null, 0, null, null);
+        MovieModel m = new MovieModel(1, "TestMovie", 60, null, null, null, null);
         MoviesLogic.UpdateMovieList(m);
         int movielength = m.Length;
 
