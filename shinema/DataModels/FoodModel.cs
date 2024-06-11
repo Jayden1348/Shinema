90ebdf32119fa7c76e8e00f0ffb660f231048029
C-G-Santos <1073607@hr.nl>
2024-06-11 10:21:50 +0200

    small mistake in FoodModel

--- shinema/DataModels/FoodModel.cs
+++ shinema/DataModels/FoodModel.cs
@@ -2,9 +2,6 @@ public class FoodModel : Consumable
 {
     public FoodModel(int id, string title, int amount, double price) : base(id, title, amount, price)
     {
-        ID = id;
-        Title = title;
-        Amount = amount;
-        Price = price;
+
     }
 }
